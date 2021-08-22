35c8d8bc1ecca135e868f0ebf9e1b1f98051b525
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2021-08-22 15:36:14 +0200

    Exercise 4 Extra credit 1 - User Data on Page Load

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,26 +5,40 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from 'utils/api-client.exercise'
+
+const getUser = async () => {
+  let user = null
+
+  const token = await auth.getToken()
+
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
   const [user, setUser] = React.useState(null)
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
-
   const logout = () => {
     auth.logout()
     setUser(null)
   }
 
-  if (user) return <AuthenticatedApp user={user} logout={logout} />
+  React.useEffect(() => {
+    getUser().then(setUser)
+  }, [])
 
-  return <UnauthenticatedApp login={login} register={register} />
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/

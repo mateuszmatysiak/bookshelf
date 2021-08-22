255a60fef6c05557cc00a7016326ecfeed88f0f4
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2021-08-22 15:52:16 +0200

    Exercise 4 Extra credit 2 - Use useAsync

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,15 +3,17 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
-import {client} from 'utils/api-client.exercise'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
 
-const getUser = async () => {
+async function getUser() {
   let user = null
 
   const token = await auth.getToken()
-
   if (token) {
     const data = await client('me', {token})
     user = data.user
@@ -21,7 +23,20 @@ const getUser = async () => {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isError,
+    isSuccess,
+    run,
+    setData: setUser,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run, setUser])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
@@ -30,15 +45,32 @@ function App() {
     setUser(null)
   }
 
-  React.useEffect(() => {
-    getUser().then(setUser)
-  }, [])
+  if (isIdle || isLoading) return <FullPageSpinner />
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isError)
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}

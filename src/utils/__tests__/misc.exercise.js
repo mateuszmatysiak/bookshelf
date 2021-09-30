0aaf28ff121949973bea0fcefa8f2bfed8c64f02
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2021-09-30 09:09:24 +0200

    Exercise 11 - Unit Testing

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date('November 22, 1996'))).toBe('Nov 96')
+})
